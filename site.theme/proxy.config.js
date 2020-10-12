044b684d4fea965f10685165eba21a1e517d58e4
Vitaly Tsaplin <tsaplin@adobe.com>
2020-10-12 16:14:43 +0200

    Add Chrome support

--- site.theme/proxy.config.js
+++ site.theme/proxy.config.js
@@ -3,7 +3,6 @@ const env = dotenv.config().parsed;
 
 module.exports = {
   url: `${env.URL}`,
-  host: `${env.URL}`.replace("https://", "").replace("http://", ""),
   css: {
     "dist": "./dist/css/theme.css",
     "url": `/conf/${env.SITE}/settings/wcm/clientlibs/theme.*.css`
