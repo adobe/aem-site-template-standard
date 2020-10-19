c731a20a4f13570c4459344f64590cea41bb8729
Vitaly Tsaplin <tsaplin@adobe.com>
2020-10-19 14:22:35 +0200

    Fix headers if implicit port is present

--- site.theme/proxy.config.js
+++ site.theme/proxy.config.js
@@ -1,8 +1,15 @@
 const dotenv = require('dotenv');
+const url = require('url');
 const env = dotenv.config().parsed;
 
+function normalizeUrl(url) {
+  const parsedUrl = url.parse(env.URL);
+  const port = (parsedUrl.port === '80' || parsedUrl.port === '443') ? '' : ':' + parsedUrl.port
+  return parsedUrl.protocol + '//' + parsedUrl.hostname + port;
+}
+
 module.exports = {
-  url: `${env.URL}`,
+  url: normalizeUrl(url),
   css: {
     "dist": "./dist/css/theme.css",
     "url": `/conf/${env.SITE}/settings/wcm/clientlibs/theme.*.css`
