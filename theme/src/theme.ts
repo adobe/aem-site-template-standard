adf87b72b9fc06b01890f2780fcac6ea909ddb1e
Gabriel Walt <gabrielwalt@users.noreply.github.com>
2021-12-16 10:20:31 +0100

    Updating theme from Webpack to Parcel (#69)
    Co-authored-by: Vlad <vlad@bailescu.ro>
    Co-authored-by: Bartosz Glowacki <bartosz.glow@gmail.com>

---
+++ theme/src/theme.ts
@@ -0,0 +1,9 @@
+
+// Stylesheets
+import "./theme.scss";
+import "./resources/images/favicon.png"
+
+// Component Javascript
+import "./components/experiencefragment/change_flag.js";
+import "./components/form-options/form_dropdown.js";
+import "./components/progressbar/progressbar.js";
