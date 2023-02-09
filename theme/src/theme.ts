f9f375983c852bf67bc403e8aab8dacc50a9e167
laura~ <lauraavanos@hotmail.com>
2023-02-09 16:54:26 +0100

    add document ready functions for header and experience fragment

--- theme/src/theme.ts
+++ theme/src/theme.ts
@@ -1,9 +1,23 @@
-
 // Stylesheets
 import "./theme.scss";
-import "./resources/images/favicon.png"
+import "./resources/images/favicon.png";
 
 // Component Javascript
-import "./components/experiencefragment/change_flag.js";
 import "./components/form-options/form_dropdown.js";
 import "./components/progressbar/progressbar.js";
+import languageNavigation from "./components/languagenavigation/languagenavigation";
+import xfHeader from "./components/experiencefragment/experiencefragment_header";
+
+const documentReadyFunctions = () => {
+  languageNavigation();
+  xfHeader();
+};
+
+if (
+  document.readyState === "complete" ||
+  document.readyState === "interactive"
+) {
+  setTimeout(documentReadyFunctions, 1);
+} else {
+  document.addEventListener("DOMContentLoaded", documentReadyFunctions);
+}
